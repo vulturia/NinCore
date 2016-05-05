5d965503907ba69a82ff6b0272e07ae0c26ed439
martijn-heil <m.heil375@gmail.com>
2016-05-05 18:58:59 +0200

    Improve /nincore listoperators to show usernames instead of CraftPlayer#toString()

--- src/main/java/tk/martijn_heil/nincore/subcommands/ListOperators.java
+++ src/main/java/tk/martijn_heil/nincore/subcommands/ListOperators.java
@@ -9,12 +9,17 @@ import tk.martijn_heil.nincore.api.command.executors.NinSubCommandExecutor;
 import tk.martijn_heil.nincore.api.exceptions.TechnicalException;
 import tk.martijn_heil.nincore.api.exceptions.ValidationException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ListOperators extends NinSubCommandExecutor
 {
 
     @Override
     public void execute(CommandSender sender, String[] args) throws ValidationException, TechnicalException
     {
-        sender.sendMessage(ChatColor.GOLD + StringUtils.join(Bukkit.getOperators(), ", "));
+        List<String> list = new ArrayList<>();
+        Bukkit.getOperators().forEach(operator -> list.add(operator.getName()));
+        sender.sendMessage(ChatColor.GOLD + StringUtils.join(list, ", "));
     }
 }
